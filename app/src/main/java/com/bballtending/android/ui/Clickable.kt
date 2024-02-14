152057d2ef52ff9151b9c55d377aabd0c58a63bc
ByeongSu Kim <kimbsu3000@gmail.com>
2024-02-14 16:26:47 +0900

    feat: 유틸 기능 추가

---
+++ app/src/main/java/com/bballtending/android/ui/Clickable.kt
@@ -0,0 +1,34 @@
+package com.bballtending.android.ui
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+@Composable
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = then(
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }
+    ) {
+        onClick()
+    }
+)
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+inline fun Modifier.noRippleCombinedClickable(
+    noinline onClick: () -> Unit,
+    noinline onLongClick: () -> Unit
+): Modifier = composed {
+    this.combinedClickable(
+        onClick = onClick,
+        onLongClick = onLongClick,
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }
+    )
+}
---
+++ app/src/main/java/com/bballtending/android/ui/preview/ComponentPreview.kt
@@ -0,0 +1,6 @@
+package com.bballtending.android.ui.preview
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+annotation class ComponentPreview
