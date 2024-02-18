51cdd6ba2a5d93b1a7665049f2d012b6efdff975
ByeongSu Kim <kimbsu3000@gmail.com>
2024-02-18 15:49:48 +0900

    build: foundation 라이브러리 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.foundation:foundation:1.3.1")
+    implementation("androidx.compose.foundation:foundation:1.6.1")
 
     testImplementation("junit:junit:4.13.2")
 
