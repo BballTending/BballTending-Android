74ac4081b786321e63baf36906c5090e056de83d
ByeongSu Kim <kimbsu3000@gmail.com>
2024-03-09 15:13:27 +0900

    build: compose version 업데이트
    from compose-bom:2023.03.00 to compose-bom:2024.02.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
